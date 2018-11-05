f5fcd2e5d8399fdc1d8c9a7afa9cfaccb716bc34
Elpoke12 <43417450+Elpoke12@users.noreply.github.com>
2018-11-05 11:24:36 -0800

    Add files via upload

---
+++ VacasVaqueras/VacasVaqueras/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VacasVaqueras
+{
+	class Program
+	{
+		public static int Sumas(IEnumerable VacasLecheras)
+		{
+			int suma = 0;
+            foreach (Object obj in VacasLecheras)
+			{
+				suma = suma + int.Parse(string.Format("{0}", obj));
+			}
+			return suma;
+		}
+		static void Main(string[] args)
+		{
+			Stack<int> VacasVaqueras = new Stack<int>();
+			Console.WriteLine(" Mazie= 2 minutos ");
+			Console.WriteLine(" Daisy= 4 minutos ");
+			Console.WriteLine(" Crazy= 10 minutos ");
+			Console.WriteLine(" Lazy= 20 minutos ");
+			Console.WriteLine(" Bob= 2 minutos");
+			VacasVaqueras.Push(2);
+			VacasVaqueras.Push(2);
+			VacasVaqueras.Push(4);
+			VacasVaqueras.Push(2);
+			VacasVaqueras.Push(20);
+			Console.WriteLine(" La suma de los minutos es de: " + Sumas(VacasVaqueras));
+			Console.ReadKey();
+		}
+	}
+}
